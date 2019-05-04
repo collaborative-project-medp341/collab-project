cf98579c708284e8b266e8f831f26521f413a9a5
SalvadorM <sronikle@gmail.com>
2019-05-03 21:49:22 -0400

    added posenet config

--- medp341-midterm/app/dev/scripts/index.js
+++ medp341-midterm/app/dev/scripts/index.js
@@ -1,3 +1,47 @@
+// canvas config
+const WIDTH = 720
+const HEIGHT = 480
+
+// posnet config
+let video 
+let poseNet 
+let poseNetIsLoaded = false 
+
+// 
+
+const pnCallback = () => {
+  console.log('pose net loaded')
+}
+
+const getCurrentPose = (poses) => {
+  if(poses.length > 0){
+    console.log(poses)
+    let 
+
+  }
+}
+
+const poseNetCapture = () => {
+  video = createCapture(VIDEO)
+
+  video.hide()
+
+  poseNet = ml5.poseNet(video, pnCallback)
+
+  poseNet.on('pose', getCurrentPose)
+}
+
+function setup(){
+  createCanvas(WIDTH, HEIGHT)
+
+  poseNetCapture()
+}
+
+function draw(){
+  image(video, 0, 0, WIDTH, HEIGHT)
+}
+
+
 $(() => {
     console.log('Page is loaded')
-}) 
+})
--- medp341-midterm/app/prod/scripts/index.js
+++ medp341-midterm/app/prod/scripts/index.js
@@ -1,110 +1,39 @@
 "use strict";
 
-//canvas settings 
+//canvas config
 var WIDTH = 720;
-var HEIGHT = 480; //posenet
+var HEIGHT = 480; //posnet config
 
 var video;
 var poseNet;
-var posNetLoaded = false; //head movement coordinates 
+var poseNetIsLoaded = false;
 
-var RightEarX = WIDTH / 2;
-var RightEarY = HEIGHT / 2;
-var LeftEarX = WIDTH / 2;
-var LeftEarY = HEIGHT / 2; //ball information
-
-var numBalls = 1;
-var spring = 0.6;
-var gravity = 0.3;
-var friction = -1;
-var balls = []; //game 
-
-var soccerIMG;
-var soccerHits = 0;
-var GAME = false;
-var endGame = false;
-var isBallHit = false;
-
-var startGame = function startGame(gameDifficulty) {
-  console.log('starting game ');
-  GAME = !GAME;
-  numBalls = gameDifficulty;
+var pnCallback = function pnCallback() {
+  console.log('pose net loaded');
 };
-
-function setup() {
-  createCanvas(WIDTH, HEIGHT);
-  soccerIMG = loadImage("../images/soccerball.png");
-
-  for (var i = 0; i < numBalls; i++) {
-    balls[i] = new Ball(random(WIDTH - WIDTH * .6, WIDTH - WIDTH * .4), 0, random(30, 70), i, balls);
-    noStroke();
-    callPoseNet();
-  }
-}
 
 var getCurrentPose = function getCurrentPose(poses) {
-  console.log(poses);
-
   if (poses.length > 0) {
-    var newLeftEarX = poses[0].pose.keypoints[3].position.x;
-    var newLeftEarY = poses[0].pose.keypoints[3].position.y - 150;
-    var newRightEarX = poses[0].pose.keypoints[4].position.x;
-    var newRightEarY = poses[0].pose.keypoints[4].position.y - 150;
-    LeftEarX = lerp(LeftEarX, newLeftEarX, 0.2);
-    LeftEarY = lerp(LeftEarY, newLeftEarY, 0.2);
-    RightEarX = lerp(RightEarX, newRightEarX, 0.2);
-    RightEarY = lerp(RightEarY, newRightEarY, 0.2);
+    console.log(poses);
   }
 };
 
-var callPoseNet = function callPoseNet() {
-  //dom video capture
+var poseNetCapture = function poseNetCapture() {
   video = createCapture(VIDEO);
-  video.hide(); //load poseNet api
-
-  poseNet = ml5.poseNet(video, cbReady); //get poseNet pose 
-
+  video.hide();
+  poseNet = ml5.poseNet(video, pnCallback);
   poseNet.on('pose', getCurrentPose);
 };
 
-var cbReady = function cbReady() {
-  posNetLoaded = true;
+var setup = function setup() {
+  createCanvas(WIDTH, HEIGHT);
+  poseNetCapture();
 };
 
-function draw() {
+var draw = function draw() {
   image(video, 0, 0, WIDTH, HEIGHT);
-
-  if (posNetLoaded && GAME) {
-    background(0);
-    image(video, 0, 0, WIDTH, HEIGHT); //head line     
-
-    stroke(153);
-    line(LeftEarX, LeftEarY, RightEarX, RightEarY); //start game 
-
-    if (!endGame) {
-      stroke(0);
-      fill(255);
-      textSize(80);
-      text(soccerHits, WIDTH - 120, 70);
-      balls.forEach(function (ball) {
-        ball.move();
-        ball.display();
-      });
-    } else {
-      stroke(0);
-      fill(255);
-      textSize(80);
-      textAlign(CENTER, CENTER);
-      text("Game Over", WIDTH / 2, HEIGHT / 2);
-    }
-  }
-}
-
-$.urlParam = function (name) {
-  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.search);
-  return results !== null ? results[1] || 0 : false;
 };
 
-$('load', function () {
-  startGame($.urlParam('mode'));
+$(function () {
+  console.log('Page is loaded');
 });
